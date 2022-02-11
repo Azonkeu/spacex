1905094268888f64042ff8ac781d5f19d55f5bf8
abrormukimov <abrormm34@gmail.com>
2022-02-11 23:34:49 +0500

    Add rocket booking

--- src/redux/rocket/rocket.js
+++ src/redux/rocket/rocket.js
@@ -7,6 +7,16 @@ export const fetchRockets = (payload) => ({
   payload,
 });
 
+export const reserveRocket = (payload) => ({
+  type: 'RESERVE_ROCKET',
+  payload,
+});
+
+export const CancelReservation = (payload) => ({
+  type: 'CANCEL_RESERVATION',
+  payload,
+});
+
 export const fetchRocketsApi = () => async (dispatch) => {
   const rockets = await axios.get('https://api.spacexdata.com/v3/rockets');
   const mapRockets = Object.entries(rockets.data).map(([id, rocket]) => {
@@ -21,10 +31,30 @@ export const fetchRocketsApi = () => async (dispatch) => {
   dispatch(fetchRockets(mapRockets));
 };
 
+export const confirmReservation = (id) => (dispatch) => {
+  dispatch(reserveRocket(id));
+};
+
+export const confirmCancelReservation = (id) => (dispatch) => {
+  dispatch(CancelReservation(id));
+};
+
 const rocketReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_ROCKETS':
       return action.payload;
+      /* eslint-disable no-labels, no-unreachable */
+    case 'RESERVE_ROCKET':
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+      /* eslint-disable no-labels, no-unreachable */
+    case 'CANCEL_RESERVATION':
+      return state.map((rocket) => {
+        if (rocket.id !== action.payload.id) return rocket;
+        return { ...rocket, reserved: false };
+      });
       /* eslint-disable no-labels, no-unreachable */
     default:
       return state;
